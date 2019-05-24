77fc69a16b62a6b5b4ed7d04871a4603b5181846
Marcus Fallor <marcusfallor@gmail.com>
2019-05-24 18:01:05 +0000

    -a

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -8,11 +8,66 @@ int main()
   // if statement to test is a is equal to 0
   if (a == 0)
   {
-    printf("a is 0\n");
+    printf("a is equal to b/n");
   }
   else
   {
-    printf("a is not 0\n");
+    printf("a is not equal to b/n");
   }
-    
+  
+
+  if (a != b) 
+  {
+    printf("a is not equal to b/n");
+  }     
+  else 
+  {
+    printf("a is equal to b/n");
+  }
+
+
+  if (a > b)
+  {
+    printf("a is greater than b/n");
+  }
+  else 
+  {
+    printf("a is not greater than b/n");
+  }
+
+  if (a >= b)
+  {
+    printf("a is greater than or equal to b/n");
+  }
+  else 
+  { 
+    printf("a is not greater than or equal to b/n");
+  }
+
+  if (a == 0 && b == 0)
+  {
+    printf("a is equal to 0 AND b/n is equal to 0");
+  }
+  else
+  {
+    printf("a is not equal to 0 AND b/n is equal to 0");
+  }
+
+  if (a == 0 || b == 0)
+  {
+    printf("a is equal to 0 OR b/n is equal to 0");
+  }
+  else
+  { 
+    printf("a is not equal to 0 OR b/n is not equal to 0");
+  }
+
+  if (!(a == 0)
+  {
+    printf("a is NOT equal to 0");
+  }
+  else
+  {
+    printf("a is equal to 0");
+  } 
 }
