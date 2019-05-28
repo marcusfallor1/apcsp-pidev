47bd9860110d7cbfec5bbca204ec9ff761d5f9f5
marcusfallor1 <50212309+marcusfallor1@users.noreply.github.com>
2019-05-27 18:14:03 -0700

    Create areaOfcircle.c

---
+++ cdemo/areaOfcircle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+float area(int a)
+{
+return (3.14 * a * a);
+}
+
+int main()
+{
+int a;
+float b;
+b = area(a);
+
+for(a = 3; a < 13; a = a + 1)
+{
+  float d = a + 0.5;
+  float e;
+  e = area(d);
+printf("Area: %f\n", e);
+}
+return 0;
+}
